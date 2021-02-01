a56bd94434cf5cbfb4b909202f5b2b505e79fa4d
kylieanyce <kylieanyce@gmail.com>
2021-02-01 09:13:07 -0600

    fixed entry list

--- scripts/JournalEntryList.js
+++ scripts/JournalEntryList.js
@@ -4,13 +4,14 @@ import { Entry } from "./JournalEntry.js"
 const entryLog = document.querySelector(".largeEntries")
 
 export const EntryListComponent = () => {
+    
     const entries = useJournalEntries()
 
     let entryHTMLrep = ""
     for (const entry of entries) {
         entryHTMLrep += Entry(entry)
         
-        entryLog.innerHTML += `
+        entryLog.innerHTML = `
             <section class="entries">
             ${entryHTMLrep}
             </section>
